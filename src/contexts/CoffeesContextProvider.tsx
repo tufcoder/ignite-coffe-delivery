daded3d877f39b8d88f1686fc9e1c7e3e3a70373
Oswaldo Castro <oswaldogpc@gmail.com>
2025-03-13 13:25:55 -0300

    Update to use localStorage

--- src/contexts/CoffeesContextProvider.tsx
+++ src/contexts/CoffeesContextProvider.tsx
@@ -24,67 +24,73 @@ interface CoffeesContextProviderProps {
 
 export function CoffeesContextProvider({ children }: CoffeesContextProviderProps) {
   const [coffees, setCoffees] = useState<CoffeeType[]>([])
-  const [orders, setOrders] = useState<CoffeeType[]>(coffees.filter(item => item.amount > 0))
+  const [orders, setOrders] = useState<CoffeeType[]>([])
   const [cityLocation, setCityLocation] = useState('')
   const [ufLocation, setUfLocation] = useState('')
+  const [dataChanged, setDataChanged] = useState(false);
+
+  const storageKey = '@coffee-delivery-1.0.0'
 
   function addAmount(coffee: CoffeeType) {
     coffee.amount++
     setCoffees(state => {
-      state.map(item => {
+      return state.map(item => {
         if (item.id === coffee.id) {
-          item.amount = coffee.amount
+          return { ...item, amount: coffee.amount }
         }
+        return item
       })
-      return state
     })
-    setOrders(coffees.filter(item => item.amount > 0))
+    setDataChanged(true);
   }
 
   function subtractAmount(coffee: CoffeeType) {
     if (coffee.amount > 1) {
       coffee.amount--
       setCoffees(state => {
-        state.map(item => {
+        return state.map(item => {
           if (item.id === coffee.id) {
-            item.amount = coffee.amount
+            return { ...item, amount: coffee.amount }
           }
+          return item
         })
-        return state
       })
-      setOrders(coffees.filter(item => item.amount > 0))
     }
+    setDataChanged(true);
   }
 
   function removeOrderItem(id: number) {
     setOrders(state => state.filter(item => item.id !== id))
     setCoffees(state => {
-      state.map(item => {
+      return state.map(item => {
         if (item.id === id) {
-          item.amount = 0
+          return { ...item, amount: 0 }
         }
+        return item
       })
-      return state
     })
+    setDataChanged(true);
   }
 
   function clean() {
-    setCoffees(state => state.map(item => {
-      item.amount = 0
-      return item
-    }))
+    setCoffees(state => state.map(item => ({ ...item, amount: 0 })))
     setOrders([])
+    setDataChanged(true);
   }
 
   async function fetchAddress(latitude: number, longitude: number) {
-    try {
-      const response = await axios.get(
-        `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
-      );
-      return response.data
-    } catch (error) {
-      console.error('Erro ao buscar endereço:', error)
-      return null
+    if (import.meta.env.MODE !== 'development') {
+      try {
+        const response = await axios.get(
+          `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+        );
+        return response.data
+      } catch (error) {
+        console.error('Erro ao buscar endereço:', error)
+        return null
+      }
+    } else {
+      return null;
     }
   }
 
@@ -96,8 +102,10 @@ export function CoffeesContextProvider({ children }: CoffeesContextProviderProps
             const latitude = position.coords.latitude
             const longitude = position.coords.longitude
             const obj = await fetchAddress(latitude, longitude)
-            setCityLocation(obj.address.city)
-            setUfLocation(obj.address['ISO3166-2-lvl4'].split('-')[1])
+            if (obj && obj.address) {
+              setCityLocation(obj.address.city)
+              setUfLocation(obj.address['ISO3166-2-lvl4'].split('-')[1])
+            }
           },
           (error) => {
             console.error("Error fetching location:", error)
@@ -110,9 +118,41 @@ export function CoffeesContextProvider({ children }: CoffeesContextProviderProps
     getLocalization()
   }, [])
 
+  function persistData(coffees: CoffeeType[], orders: CoffeeType[]) {
+    const dataToStore = { coffees, orders }
+    localStorage.setItem(storageKey, JSON.stringify(dataToStore))
+  }
+
   useEffect(() => {
-    setCoffees(data)
+    const storedData = localStorage.getItem(storageKey)
+    if (storedData) {
+      console.log('storage')
+      try {
+        const parsedData = JSON.parse(storedData)
+        setCoffees(parsedData.coffees || [])
+        setOrders(parsedData.orders || [])
+      } catch (error) {
+        console.error('Error parsing localStorage data', error)
+        setCoffees(data)
+        setOrders([])
+      }
+    } else {
+      console.log('seed')
+      setCoffees(data)
+      setOrders([])
+    }
   }, [])
+
+  useEffect(() => {
+    if (dataChanged) {
+      persistData(coffees, orders);
+      setDataChanged(false);
+    }
+  }, [coffees, orders, dataChanged])
+
+  useEffect(() => {
+    setOrders(coffees.filter(item => item.amount > 0))
+  }, [coffees])
 
   return (
     <CoffeesContext.Provider
